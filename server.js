7219610cafa48f90c7393ae0fa561866bcb3ac1d
lachmish <shlomi.lachmish@sap.com>
2017-06-01 05:02:44 +0000

    api date to unix

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const express = require('express')
+const moment = require('moment')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+var oDate = new Date();
+var jsonDate = {};
+jsonDate.humenDate = oDate.getDate();
+jsonDate.unixDate = 123321123;
+
+app.get('/:date', function (req, res) {
+  var resJson = {
+    date:null,
+    unix:null
+  }
+  //res.send(JSON.stringify(jsonDate));
+  if (moment(req.params.date, "DD-MM-YYYY", true).isValid()){ //we have a humen date detected
+    resJson.date = req.params.date;
+    var hDate =  moment(req.params.date, "DD-MM-YYYY");
+    //build a unix time 
+    var uTime = (hDate.valueOf() / 1000).toString(); 
+    resJson.unix = uTime;
+  }
+  if (moment(req.params.date, "X", true).isValid()){ //we have a unix date detected
+    resJson.unix = req.params.date;
+    var uDate =  moment(req.params.date, "X");
+    //build a humen time 
+    var hTime = moment(uDate).format("DD-MM-YYYY"); 
+    resJson.date = hTime;
+  }
+  res.send(resJson);
 })
 
 app.listen(process.env.PORT, function () {
